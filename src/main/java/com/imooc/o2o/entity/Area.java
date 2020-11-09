ebdb2e7ccfcfc551286f429121ef7cceffea56cf
susansys <sun447@purdue.edu>
2020-11-09 20:14:01 +0800

    created entities + sql script
    create entities
    create sql script by using MySql workbench

---
+++ src/main/java/com/imooc/o2o/entity/Area.java
@@ -0,0 +1,56 @@
+package com.imooc.o2o.entity;
+
+import java.util.Date;
+
+public class Area {
+    //ID
+    private Integer areaId;
+    //name
+    private String areaName;
+    //priority
+    private Integer priority;
+    //create time
+    private Date createTime;
+    //update time
+    private Date lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+}
