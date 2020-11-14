78eaddd8f130840d04273637895e92bcd3419b2a
susansys <sun447@purdue.edu>
2020-11-14 21:56:19 +0800

    Log files are in this directory:  C:\Users\username\AppData\Local\JetBrains\IntelliJIdea2020.2\tomcat

--- src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/imooc/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.imooc.o2o.web.superadmin;
 
 import com.imooc.o2o.entity.Area;
 import com.imooc.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,14 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
     @RequestMapping(value ="/listarea", method =RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listArea(){
+        logger.info("===Start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<String, Object>();
         List<Area> list = new ArrayList<Area>();
         try{
@@ -32,6 +37,10 @@ public class AreaController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        logger.error("test error!");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]",endTime - startTime);
+        logger.info("===end===");
         return modelMap;
     }
 }
